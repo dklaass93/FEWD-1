573c3ee6c978cab308f3f739e6af8cccb1c14787
dklaass93 <davidklaassen@Macintosh-10.local>
2016-02-08 15:30:24 -0500

    Commit 02/08
    Commit for PDF.js

--- Websites/Law Site Samples/Sample #1/js/index.js
+++ Websites/Law Site Samples/Sample #1/js/index.js
@@ -1,8 +1,25 @@
 $(document).ready(function(){
 
 $("#contact").click(function() {
-	$("#connect-form-wrap").slideToggle();
+	$("#outer-connect-form-wrapper").slideToggle();
+	$("#connect-form-wrapper").slideToggle();
+	$(".CTA").hide();
+	$(".CTA-buttons").hide();
 });
 
+$("#connect-form-close").click(function(){
+	$("#outer-connect-form-wrapper").fadeOut(800);
+	$('#connect-form-wrapper').fadeOut(800);
+	$(".CTA").show();
+	$(".CTA-buttons").show();
+});
+
+// $("#outer-connect-form-wrapper").click(function(){
+// 	$("#outer-connect-form-wrapper").fadeOut(800);
+// 	$('#connect-form-wrapper').fadeOut(800);
+// 	$(".CTA").show();
+// 	$(".CTA-buttons").show();
+// });
 
 })
+
